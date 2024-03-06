82cd314ba59cb397ca174c3fbb5bcc4420bafe7c
HAJI <143566329+haji8-thehaji@users.noreply.github.com>
2024-03-06 09:54:51 +0900

    Create w1s2.cs

---
+++ example/w1s2.cs
@@ -0,0 +1,18 @@
+// step 2. 자료형 테스트
+Console.WriteLine("====== === step 2. 자료형 테스트 === ======");
+int intVariable = 10;
+char charVariable = 'a';
+float floatVariable = 3.123456789f;
+double doubleVariable = 3.1234567890;
+bool boolVariable = true;
+string stringVariable = "Hello, World";
+
+
+Console.WriteLine(intVariable);
+Console.WriteLine(charVariable);
+Console.WriteLine(floatVariable);
+Console.WriteLine(doubleVariable);
+Console.WriteLine(boolVariable);
+Console.WriteLine(stringVariable);
+
+Console.ReadLine();
