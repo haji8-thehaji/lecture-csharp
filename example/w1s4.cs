4b85a8e5dc581eaa894042b345f19c1d637ce99d
HAJI <143566329+haji8-thehaji@users.noreply.github.com>
2024-03-06 11:01:47 +0900

    Create w1s4.cs

---
+++ example/w1s4.cs
@@ -0,0 +1,22 @@
+// step 4. 연산자
+Console.WriteLine("====== === step 4. 연산자 === ======");
+
+sbyte bitA = 64;
+Console.WriteLine(bitA << 1);
+Console.WriteLine(bitA >> 1);
+
+Console.WriteLine(bitA + 1);
+Console.WriteLine(bitA - 1);
+
+sbyte bitB = 10;
+Console.WriteLine(bitB++);
+Console.WriteLine(bitB);
+Console.WriteLine(++bitB);
+
+sbyte bitC = 10;
+Console.WriteLine(--bitC);
+Console.WriteLine(bitC);
+Console.WriteLine(bitC--);
+
+
+Console.ReadLine();
