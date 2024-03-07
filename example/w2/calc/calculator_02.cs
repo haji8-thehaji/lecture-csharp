3fd154a4905ee5d8c6c6fc61869517f235bb30b9
HAJI <143566329+haji8-thehaji@users.noreply.github.com>
2024-03-07 14:50:02 +0900

    Update calculator_02.cs

--- example/w2/calc/calculator_02.cs
+++ example/w2/calc/calculator_02.cs
@@ -1,35 +1,43 @@
 
-int in_num1 = 0, in_num2 = 0, sum = 0;
+int in_num1 = 0, in_num2 = 0, result = 0;
 string in_oper;//사칙연산 연산자 
 
 Console.WriteLine("첫 번째 숫자를 입력하세요");
-//입력받은 문자열을 int형으로 형변환 후 in_num1에 저장 
+// Console.ReadLine()
+// int.Parse 스트링을 int로 형변환함
 in_num1 = int.Parse(Console.ReadLine());
-//입력받은 문자열을 int형으로 형변환 후 in_num2에 저장 
 Console.WriteLine("두 번째 숫자를 입력하세요");
 in_num2 = int.Parse(Console.ReadLine());
-//입력받은 문자열을 in_oper에 저장 
-Console.WriteLine("사친연산 연산자를 입력하세요");
-in_oper = Console.ReadLine();
 
-    //문자열이므로 " "로 작성 
+// *   -  + 
+Console.WriteLine("사칙연산 연산자를 입력하세요");
+in_oper = (String)Console.ReadLine();
+
 switch (in_oper)
 {
     case "+":
-        sum = in_num1 + in_num2;
-        Console.WriteLine("{0} + {1} = {2}", in_num1, in_num2, sum);
+        result = in_num1 + in_num2;
+        Console.WriteLine(in_num1 + " + " + in_num2 + "  = " + result);
         break;
     case "-":
-        sum = in_num1 - in_num2;
-        Console.WriteLine("{0} - {1} = {2}", in_num1, in_num2, sum);
+        result = in_num1 - in_num2;
+        Console.WriteLine($"{in_num1} - {in_num2} = {result}");
         break;
     case "*":
-        sum = in_num1 * in_num2;
-        Console.WriteLine("{0} * {1} = {2}", in_num1, in_num2, sum);
+        result = in_num1 * in_num2;
+        Console.WriteLine("{0} * {1} = {2}", in_num1, in_num2, result);
         break;
     case "/":
-        sum = in_num1 / in_num2;
-        Console.WriteLine("{0} / {1} = {2}", in_num1, in_num2, sum);
+        try
+        {
+            result = in_num1 / in_num2;
+            Console.WriteLine("{0} / {1} = {2}", in_num1, in_num2, result);
+
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("0으로 나눌수 없습니다");
+        }
         break;
     default:
         Console.WriteLine("사칙연산자를 잘못 입력하였습니다.");
